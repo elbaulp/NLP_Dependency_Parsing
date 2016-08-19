836846fc147028f86ec84f17635ecb4a2e7106ce
Alejandro Alcalde <algui91@gmail.com>
2016-08-19 11:33:23 +0200

    Added constants for data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,22 @@
+
+
+/*
+ *     Main.scala is part of grado_informatica_tfg_naturallanguageprocessing (grado_informatica_TFG_NaturalLanguageProcessing).
+ *
+ *     grado_informatica_TFG_NaturalLanguageProcessing is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     grado_informatica_TFG_NaturalLanguageProcessing is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with grado_informatica_TFG_NaturalLanguageProcessing.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /*
  *     Main.scala is part of grado_informatica_tfg_naturallanguageprocessing (grado_informatica_TFG_NaturalLanguageProcessing).
  *
@@ -19,7 +38,19 @@
   * Created by Alejandro Alcalde <contacto@elbauldelprogramador.com> on 8/18/16.
   */
 object Main extends App {
-//  val DirPath =
+  val DataSourcePath = "/data/spanish"
+  val ModelsPath = "/models"
+  val TrainSentencesFile = "/es_ancora-converted-train"
+  val TestSentencesFile = "/ancora-test-one-sentence"
+
+  val TrainSet = getClass.getResourceAsStream(DataSourcePath + TrainSentencesFile)
+  val TestSet = getClass.getResourceAsStream(DataSourcePath + TestSentencesFile)
+
+  val TrainSetLines = scala.io.Source.fromInputStream(TrainSet).getLines
+  val TestSetLines = scala.io.Source.fromInputStream(TestSet).getLines
+
+  //  TrainSetLines.foreach(System.out.println)
+  //  TestSetLines.foreach(System.out.println)
 }
 
 
